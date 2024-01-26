66fc613420c19a2ee3981c7b5887d6e6c58b4888
Patricia Carrasco <patri.carrasco@gmail.com>
2024-01-26 11:30:40 +0100

    doing load_fact.py

--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -1,4 +1,5 @@
 from airflow.hooks.postgres_hook import PostgresHook
+from airflow.hooks.aws_hook import AwsHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
@@ -9,14 +10,26 @@ class LoadFactOperator(BaseOperator):
     @apply_defaults
     def __init__(self,
                  # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 aws_credentials_id="",
+                 table="",
+                 s3_bucket="",
+                 s3_key="",
+                 log_json_path="",
+                 delimiter=",",
+                 ignore_headers=1,
                  *args, **kwargs):
 
         super(LoadFactOperator, self).__init__(*args, **kwargs)
         # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.table = table
+        self.redshift_conn_id = redshift_conn_id
+        self.s3_bucket = s3_bucket
+        self.s3_key = s3_key
+        self.log_json_path = log_json_path
+        self.delimiter = delimiter
+        self.ignore_headers = ignore_headers
+        self.aws_credentials_id = aws_credentials_id
 
     def execute(self, context):
         self.log.info('LoadFactOperator not implemented yet')
